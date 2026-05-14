021976c357bde6bc6c2b9215b4d9113fe9c62d73
elgiz20002000 <ext-ismailov_ye@bi.group>
2026-05-14 14:34:48 +0400

    Add Header component and integrate routing in App component

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
+import { BrowserRouter } from 'react-router-dom';
 
 export default function App() {
     return (
-        <div>
-            <MarketingApp />
-        </div>
+        <BrowserRouter>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div>
+        </BrowserRouter>
     );
 }
