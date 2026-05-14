4f829f8b4b888dacd8109c3ffa1a42935c4f17ad
elgiz20002000 <ext-ismailov_ye@bi.group>
2026-05-14 15:16:39 +0400

    Refactor App components to use StylesProvider with custom class name generation for improved styling consistency

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { StylesProvider } from '@material-ui/core/styles';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Pricing, Landing } from './components';
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'ma',
+});
 
 export default function App() {
     return (
         <div>
-            <StylesProvider>
+            <StylesProvider generateClassName={generateClassName}>
                 <BrowserRouter>
                     <Switch>
                         <Route path="/pricing" component={Pricing} />
